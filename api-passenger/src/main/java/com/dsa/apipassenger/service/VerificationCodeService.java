e55eeb7d07819a36815ef5103bc8d8542c478d87
和联胜坐馆李家源 <duanshiao@outlook.com>
2025-01-18 11:29:40 +0800

    250118 feign可变参数控制验证码位数，api-passenger调用service-verificationcode实现功能

--- api-passenger/src/main/java/com/dsa/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/dsa/apipassenger/service/VerificationCodeService.java
@@ -1,15 +1,27 @@
 package com.dsa.apipassenger.service;
 
+import com.dsa.apipassenger.remote.ServiceVerificationCodeClient;
+import com.dsa.internalcommon.dto.ResponseResult;
+import com.dsa.internalcommon.responese.NumberCodeResponse;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VerificationCodeService {
-    public String generatorCode(String passengerPhone){
-        System.out.println("请用验证码，获取验证码");
-        String code = "1111";
 
-        System.out.println("存入redis");
+    @Autowired
+    ServiceVerificationCodeClient serviceVerificationCodeClient;
+
+    public String generatorCode(String passengerPhone){
+
+
+        ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVerificationCodeClient.getNumberCode(5);
+        int NumberCode = numberCodeResponse.getData().getNumberCode();
+
+        System.out.println("获取验证码:" + NumberCode);
+//        System.out.println("获取验证码");
+//        System.out.println("存入redis");
 
         JSONObject result = new JSONObject();
         result.put("code", 1);
