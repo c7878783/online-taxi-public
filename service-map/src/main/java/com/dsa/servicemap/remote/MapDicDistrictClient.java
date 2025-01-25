2eba81d2ce9990cf530a47d285be940fc188bc11
和联胜坐馆李家源 <duanshiao@outlook.com>
2025-01-25 18:27:19 +0800

    请求地图地区字典

---
+++ service-map/src/main/java/com/dsa/servicemap/remote/MapDicDistrictClient.java
@@ -0,0 +1,37 @@
+package com.dsa.servicemap.remote;
+
+import com.dsa.internalcommon.constant.AmapConfigConstants;
+import com.dsa.internalcommon.dto.ResponseResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class MapDicDistrictClient {
+
+    @Value("${amap.key}")
+    private String amapKey;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String dicDistrict(String keywords) {
+        //拼装请求url
+        StringBuilder url = new StringBuilder();
+        url.append(AmapConfigConstants.DISTRICT_URL)
+                .append("?")
+                .append("keywords="+keywords)
+                .append("&")
+                .append("subdistrict=3")
+                .append("&")
+                .append("key="+amapKey);
+        //解析结果
+        ResponseEntity<String> forEntity = restTemplate.getForEntity(url.toString(), String.class);
+
+        //插入数据库
+
+        return forEntity.getBody();
+    }
+}
--- service-map/src/main/java/com/dsa/servicemap/service/DicDistrictService.java
+++ service-map/src/main/java/com/dsa/servicemap/service/DicDistrictService.java
@@ -2,8 +2,11 @@ package com.dsa.servicemap.service;
 
 import com.dsa.internalcommon.constant.AmapConfigConstants;
 import com.dsa.internalcommon.dto.ResponseResult;
+import com.dsa.servicemap.remote.MapDicDistrictClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class DicDistrictService {
@@ -11,20 +14,21 @@ public class DicDistrictService {
     @Value("${amap.key}")
     private String amapKey;
 
+    @Autowired
+    MapDicDistrictClient mapDicDistrictClient;
+
+
+
     public ResponseResult initDistrict(String keywords) {
-        //拼装请求url
-        StringBuilder url = new StringBuilder();
-        url.append(AmapConfigConstants.DISTRICT_URL)
-                .append("?")
-                .append("keywords+"+keywords)
-                .append("&")
-                .append("subdistrict=3")
-                .append("&")
-                .append("key="+amapKey);
+        //请求地图
+        String dicDistrict = mapDicDistrictClient.dicDistrict(keywords);
+        System.out.println(dicDistrict);
+
+
         //解析结果
 
         //插入数据库
-        return null;
+        return ResponseResult.success();
 
     }
 }
