4c4e2ed5b7b03abe1aa2ca607a6d5bfc8e94915f
和联胜坐馆李家源 <duanshiao@outlook.com>
2025-01-27 18:33:03 +0800

    apiboss调用实现service-user司机信息的修改

--- api-boss/src/main/java/com/dsa/apiboss/service/DriverUserService.java
+++ api-boss/src/main/java/com/dsa/apiboss/service/DriverUserService.java
@@ -22,6 +22,6 @@ public class DriverUserService {
 
     public ResponseResult updateDriverUser(DriverUser driverUser) {
 
-        return
+        return serviceDriverUserClient.updateDriverUser(driverUser);
     }
 }
