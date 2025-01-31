7cca976c62c4e20479ee4c2e37265baab1855fe7
和联胜坐馆李家源 <duanshiao@outlook.com>
2025-01-31 19:20:00 +0800

    api-passenger下单接口接受参数代码

---
+++ internal-common/src/main/java/com/dsa/internalcommon/request/OrderRequest.java
@@ -0,0 +1,26 @@
+package com.dsa.internalcommon.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrderRequest {
+    //订单发起地区域代码
+    private String address;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime departTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime orderTime;
+    private String departure;
+    private String depLongitude;
+    private String depLatitude;
+    private String destination;
+    private String desLongitude;
+    private String desLatitude;
+    //坐标加密表示:1:gcj-02,2:wgs84,3:bd-09,4:cgcs2000北斗,0:其他
+    private Integer encrypt;
+    private String fareType;
+
+}
