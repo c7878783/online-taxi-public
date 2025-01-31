48b3b8918dd64974808660addea5d17f0dcebb09
和联胜坐馆李家源 <duanshiao@outlook.com>
2025-01-31 10:59:33 +0800

    api-driver上传位置功能，调用service-map上传轨迹点

--- api-driver/src/main/java/com/dsa/apidriver/service/PointService.java
+++ api-driver/src/main/java/com/dsa/apidriver/service/PointService.java
@@ -1,9 +1,11 @@
 package com.dsa.apidriver.service;
 
 import com.dsa.apidriver.remote.ServiceDriverUserClient;
+import com.dsa.apidriver.remote.ServiceMapClient;
 import com.dsa.internalcommon.dto.ResponseResult;
 import com.dsa.internalcommon.pojo.Car;
 import com.dsa.internalcommon.request.ApiDriverPointRequest;
+import com.dsa.internalcommon.request.PointRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,17 +15,23 @@ import java.awt.geom.RectangularShape;
 public class PointService {
     @Autowired
     ServiceDriverUserClient serviceDriverUserClient;
+    @Autowired
+    ServiceMapClient serviceMapClient;
 
     public ResponseResult upload(ApiDriverPointRequest apiDriverPointRequest){
         //获取carId;
         Long carId = apiDriverPointRequest.getCarId();
-        //通过carId 获取tid、trid、调用service-driveuser的接口
+        //通过carId 获取tid、trid、调用service-drive-user的接口
         ResponseResult<Car> carById = serviceDriverUserClient.getCarById(carId);
         Car car = carById.getData();
         String tid = car.getTid();
         String trid = car.getTrid();
-        //调用地图去上传
+        //调用service-map去上传
+        PointRequest pointRequest = new PointRequest();
+        pointRequest.setTid(tid);
+        pointRequest.setTrid(trid);
+        pointRequest.setPoints(apiDriverPointRequest.getPoints());
 
-        return null;
+        return serviceMapClient.upload(pointRequest);
     }
 }
