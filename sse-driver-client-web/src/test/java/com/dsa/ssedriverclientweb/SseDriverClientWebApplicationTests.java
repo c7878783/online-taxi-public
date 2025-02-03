d15fe0e36faeb8bcceec3fb81892fa838fb0589c
和联胜坐馆李家源 <duanshiao@outlook.com>
2025-02-03 16:31:49 +0800

    司机客户端和后端的建立，监听及推送、监听链接关闭

---
+++ sse-driver-client-web/src/test/java/com/dsa/ssedriverclientweb/SseDriverClientWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsa.ssedriverclientweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseDriverClientWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
