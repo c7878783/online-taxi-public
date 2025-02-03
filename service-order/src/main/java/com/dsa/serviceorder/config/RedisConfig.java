879c5bf74d721d89361fb402f1709032a3a1affb
和联胜坐馆李家源 <duanshiao@outlook.com>
2025-02-03 11:35:35 +0800

    并发式锁redisson解决方案

--- service-order/src/main/java/com/dsa/serviceorder/config/RedisConfig.java
+++ service-order/src/main/java/com/dsa/serviceorder/config/RedisConfig.java
@@ -3,16 +3,25 @@ package com.dsa.serviceorder.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RedisConfig {
 
+    private String protocol = "redis://";
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private String redisPort;
+
     @Bean
     public RedissonClient redissonClient(){
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setDatabase(0);
+        config.useSingleServer().setAddress(protocol + redisHost + ":" + redisPort).setDatabase(0);
 
         return Redisson.create(config);
     }
