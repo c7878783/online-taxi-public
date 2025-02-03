1cf16ca5008de12efb379191afc5c59616601eff
和联胜坐馆李家源 <duanshiao@outlook.com>
2025-02-03 16:44:08 +0800

    司机客户端和后端的建立，消息监听以及监听链接关闭

--- service-sse-push/src/main/java/com/dsa/ssedriverclientweb/SseDriverClientWebApplication.java
+++ service-sse-push/src/main/java/com/dsa/ssedriverclientweb/SseDriverClientWebApplication.java
@@ -2,8 +2,10 @@ package com.dsa.ssedriverclientweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SseDriverClientWebApplication {
 
     public static void main(String[] args) {
