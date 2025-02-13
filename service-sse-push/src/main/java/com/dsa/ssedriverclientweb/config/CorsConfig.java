c55c7cf103c4018a2059c656d0333403ea25a9d3
和联胜坐馆李家源 <duanshiao@outlook.com>
2025-02-13 19:15:37 +0800

    403forbidden错误，上网查询后发现是需要配置CorsConfig

--- service-sse-push/src/main/java/com/dsa/ssedriverclientweb/config/CorsConfig.java
+++ service-sse-push/src/main/java/com/dsa/ssedriverclientweb/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3001")  // 允许前端域名
+                        .allowedOrigins("*")  // 允许前端域名
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Access-Control-Allow-Origin")
