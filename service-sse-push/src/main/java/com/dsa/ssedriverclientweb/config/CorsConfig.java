855c17ff7c91f75c594eba0d12afa2025db19f3b
和联胜坐馆李家源 <duanshiao@outlook.com>
2025-02-13 19:01:43 +0800

    403forbidden错误，上网查询后发现是需要配置CorsConfig

---
+++ service-sse-push/src/main/java/com/dsa/ssedriverclientweb/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.dsa.ssedriverclientweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3001")  // 允许前端域名
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Access-Control-Allow-Origin")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
